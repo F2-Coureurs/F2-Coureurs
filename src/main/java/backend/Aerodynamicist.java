756eff32252dff340e686823d9c91a90ebbcfc27
jiayinysj <jiayinysj@gmail.com>
2016-11-26 21:15:06 +0100

    Add class aerodynamicist and its test with some changes

---
+++ src/main/java/backend/Aerodynamicist.java
@@ -0,0 +1,13 @@
+package backend;
+
+public class Aerodynamicist extends Staff{
+
+    /**
+     *  Create a new aerodynamicisi
+     *
+     *  @param name the name of the staff member
+     *  @param salary the salary per race
+     */
+    public Aerodynamicist(String name, int salary) { super(name, salary);}
+}
+
